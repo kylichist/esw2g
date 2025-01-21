eea79882f251973513f886bee87246b384e382eb
Petr Pimenov <pimenovpetya@gmail.com>
2025-01-22 00:37:15 +0300

    Вручную уменьшена громкость звука

--- src/common/utils/play-sound.ts
+++ src/common/utils/play-sound.ts
@@ -2,6 +2,7 @@ import { Sounds } from '../enums';
 
 export const playSound = (sound: Sounds): Promise<void> => new Promise((resolve) => {
   const audio = new Audio(`./sounds/${sound}`);
+  audio.volume = 0.1
 
   audio.addEventListener('canplaythrough', () => {
     audio.play();
